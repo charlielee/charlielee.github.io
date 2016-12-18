d0a6e8dd32f00bf9fab06c662194d456b6d5cbed
Charlie Lee <charlielee735@gmail.com>
2016-12-18 16:13:50 +0000

    Add deployment scripts

---
+++ deploy.bat
@@ -0,0 +1,4 @@
+jekyll build
+cd _site
+git commit -a -m "Update Jekyll build"
+git push -f
---
+++ setup.bat
@@ -0,0 +1,2 @@
+git clone https://github.com/BoatsAreRockable/BoatsAreRockable.github.io.git
+ren BoatsAreRockable.github.io _site
