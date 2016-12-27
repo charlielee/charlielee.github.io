f0fb35acda678d4156a413ed67a3cdea2fa56303
Charlie Lee <charlielee735@gmail.com>
2016-12-27 22:25:55 +0100

    Initial _site subtree commit

---
+++ _site/assets/main.js
@@ -0,0 +1,17 @@
+/*!
+  JavaScript used on the entire website.
+*/
+
+var mobileToggle = document.getElementById("mobile-menu-toggle"),
+    menuList     = document.getElementById("menu-list");
+
+mobileToggle.addEventListener("click", toggleMenu);
+window.addEventListener("load", toggleMenu);
+
+/**
+ * Toggles the display of the navigation menu in mobile view
+ */
+function toggleMenu() {
+  menuList.classList.toggle("hidden");
+}
+
