cf1b9cf5c5d3346891d65e60e5e269d11feffe53
Charlie Lee <charlielee735@gmail.com>
2017-01-28 15:26:39 +0000

    Fix mobile navigation

--- assets/main.js
+++ assets/main.js
@@ -6,12 +6,11 @@ var mobileToggle = document.getElementById("mobile-menu-toggle"),
     menuList     = document.getElementById("menu-list");
 
 mobileToggle.addEventListener("click", toggleMenu);
-window.addEventListener("load", toggleMenu);
 
 /**
  * Toggles the display of the navigation menu in mobile view
  */
 function toggleMenu() {
-  menuList.classList.toggle("hidden");
+  menuList.classList.toggle("visible");
 }
 
